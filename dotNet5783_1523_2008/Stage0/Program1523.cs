2ce6a8aa2eac50193b23b4aa9f89c40735decdbf
Marcos <mordechay0@gmail.com>
2022-10-27 17:28:39 +0300

    Targil0(Try)

---
+++ dotNet5783_1523_2008/Stage0/Program1523.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System;
+namespace Targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            welcome2008();
+            welcome1523();
+            Console.ReadKey();
+        }
+
+        static partial void welcome1523();
+
+        private static void welcome2008()
+        {
+        
+        }
+
+
+
+    }
+}
