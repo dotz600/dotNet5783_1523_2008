b4446d56e5d93be7cb7382bf109f2d989badcf59
dotz6000 <ohev.david@gmail.com>
2022-10-29 22:50:15 +0300

    change 1

--- dotNet5783_1523_2008/Stage0/Program2008.cs
+++ dotNet5783_1523_2008/Stage0/Program2008.cs
@@ -14,7 +14,7 @@ namespace Stage0
         }
         private static void welcome2008()
         {
-            Console.WriteLine("hello my name shmuel");
+            Console.WriteLine("hi");
         }
 
     }
