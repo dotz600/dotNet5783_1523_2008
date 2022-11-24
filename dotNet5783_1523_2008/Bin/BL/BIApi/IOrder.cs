211ab44ef73d95c37130c8fe2d4495a16ed67a2b
dotz6000 <ohev.david@gmail.com>
2022-11-24 12:00:09 +0200

    iu

--- dotNet5783_1523_2008/Bin/BL/BIApi/IOrder.cs
+++ dotNet5783_1523_2008/Bin/BL/BIApi/IOrder.cs
@@ -16,9 +16,9 @@ namespace BIApi;
 public interface IOrder
 {
     IEnumerable<BO.OrderForList> ReadAll();//returns list of BO.Order
-    BO. Read(int orderId);//returns single BO.Order
-    UpdateDelivery(int orderId);//update status, and returns BO.Order
-    UpdateShipping(int orderId);//update status, and returns BO.Order
+    BO.Order Read(int orderId);//returns single BO.Order
+    BO.Order UpdateDelivery(int orderId);//update status, and returns BO.Order
+    BO.Order UpdateShipping(int orderId);//update status, and returns BO.Order
     BO.OrderTracking TrackingOrder(int orderId);//returns current status, and list of events that were occurred in these order
 <<<<<<< HEAD
     void buildItemsList(ref List<BO.OrderItem> l, int id);//Building Items list according to a given ID
