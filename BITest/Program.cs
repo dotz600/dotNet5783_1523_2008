709f4baab0aed491fc83ca074521d19c6efc7205
Marcos <mordechay0@gmail.com>
2022-11-28 18:42:26 +0200

    cahnge

--- BITest/Program.cs
+++ BITest/Program.cs
@@ -3,7 +3,7 @@ using BlImplementation;
 using BO;
 
 namespace BlTest;
-//its posiblle// we can do it !!!onr last check all very hard
+//its posiblle// we can do it !!!onr last check all very good
 internal class Program
 {
     static void Main(string[] args)
