6ef7c6ca87ac98cf8959922d1e7cdcae76fcf03c
dotz6000 <ohev.david@gmail.com>
2023-01-16 10:24:27 +0200

    stimulation worker

--- PL/StartSimulatorWindow.xaml.cs
+++ PL/StartSimulatorWindow.xaml.cs
@@ -1,27 +1,58 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
+using System.Diagnostics;
+using System.Threading;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
-namespace PL
+namespace PL;
+
+/// <summary>
+/// Interaction logic for StartSimulatorWindow.xaml
+/// </summary>
+public partial class StartSimulatorWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for StartSimulatorWindow.xaml
-    /// </summary>
-    public partial class StartSimulatorWindow : Window
+
+    private Stopwatch stopWatch;
+
+    private bool isTimerRun;
+
+    BackgroundWorker timerworker;
+
+    public StartSimulatorWindow()
     {
-        public StartSimulatorWindow()
+        InitializeComponent();
+        stopWatch = new Stopwatch();
+        timerworker = new BackgroundWorker();
+
+        timerworker.DoWork += ClockRun;
+        timerworker.ProgressChanged += ClockTxt;
+
+        timerworker.WorkerReportsProgress = true;
+        stopWatch.Start();
+        isTimerRun = true;
+
+
+        timerworker.RunWorkerAsync();
+    }
+
+
+    private void ClockRun(object? sender, DoWorkEventArgs? e)
+    {
+        stopWatch.Start();
+        while (isTimerRun)
         {
-            InitializeComponent();
+            timerworker.ReportProgress(231);
+            Thread.Sleep(1000);
         }
+    }
+    private void ClockTxt(object? sender, ProgressChangedEventArgs? e)
+    {
+        string timerText = stopWatch.Elapsed.ToString();
+        timerText = timerText.Substring(0, 8);
+        this.timerTextBlock.Text = timerText;
+    }
+
+    private void stopTimerButton_Click(object sender, RoutedEventArgs e)
+    {
+        this.Close();
     }
 }
